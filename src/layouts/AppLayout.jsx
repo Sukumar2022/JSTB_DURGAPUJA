ebd9b9703df6ba775cf1069ab22c37a6d40a136b
Sukumar Mondal <104411181+Sukumar2022@users.noreply.github.com>
2026-09-24 20:55:32 +0530

    Update AppLayout.jsx

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
-import Navbar from '../component/Navbar'
-import Footer from '../component/Footer'
-import { Outlet } from 'react-router-dom'
+import React from 'react';
+import { Outlet, useNavigation } from 'react-router-dom';
+import Navbar from '../component/Navbar';
+import Footer from '../component/Footer';
+import PujaLoader from '../component/PujaLoader';
 
 const AppLayout = () => {
-  return (
-    <>
-        <Navbar/>
-        <Outlet/>
-        <Footer/>
-    </>
-  )
-}
+  const navigation = useNavigation();
 
-export default AppLayout
+  // Checks if React Router is actively loading data or fetching a route
+  const isLoading = navigation.state === 'loading';
+
+  return (
+    <div className="min-h-screen flex flex-col bg-[#4a0303] text-[#fbeee0] selection:bg-[#e5a93c] selection:text-[#4a0303]">
+      {/* Display Fullscreen Loader during Page Transitions */}
+      {isLoading && <PujaLoader text="Preparing Celebration..." />}
+
+      {/* Header Navigation */}
+      <Navbar />
+
+      {/* Dynamic Page Content Wrapper */}
+      <main className="flex-grow w-full relative">
+        <Outlet />
+      </main>
+
+      {/* Footer Section */}
+      <Footer />
+    </div>
+  );
+};
+
+export default AppLayout;
